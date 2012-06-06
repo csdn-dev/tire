965aaebceee0a8e0e6cb5f140d2295bbafc1b4db
wangxz <wangxz@csdn.net>
2012-06-06 14:33:59 +0800

    fix log

--- lib/tire/index.rb
+++ lib/tire/index.rb
@@ -91,7 +91,7 @@ module Tire
     end
 
     def flush
-      @response = Configuration.client.put("#{url}/_flush", {})
+      @response = Configuration.client.put("#{url}/_flush", "")
       @response.success?
 
     ensure
@@ -100,170 +100,12 @@ module Tire
     end
 
     def refresh
-      @response = Configuration.client.put("#{url}/_refresh", {})
+      @response = Configuration.client.put("#{url}/_refresh", "")
       @response.success?
 
     ensure
-      curl = %Q|curl -X PUT #{url}/_flush|
-      logged('FLUSH', curl)
-    end
-
-    def settings
-      @response = Configuration.client.get("#{url}/_settings")
-      MultiJson.decode(@response.body)[@name]['settings']
-    end
-
-    def import(klass_or_collection, options={})
-      case
-      when method = options.delete(:method)
-        options = {:page => 1, :per_page => 1000}.merge options
-        while documents = klass_or_collection.send(method.to_sym, options.merge(:page => options[:page])) \
-            and documents.to_a.length > 0
-
-          documents = yield documents if block_given?
-
-          bulk_store documents, options
-          options[:page] += 1
-        end
-
-      when klass_or_collection.respond_to?(:map)
-        documents = block_given? ? yield(klass_or_collection) : klass_or_collection
-        bulk_store documents, options
-
-      else
-        raise ArgumentError, "Please pass either an Enumerable compatible class, or a collection object" +
-          "with a method for fetching records in batches (such as 'paginate')"
-      end
-    end
-
-    def reindex(name, options={}, &block)
-      new_index = Index.new(name)
-      new_index.create(options) unless new_index.exists?
-
-      Search::Scan.new(self.name, &block).each do |results|
-        new_index.bulk_store results.map do |document|
-          document.to_hash.except(:type, :_index, :_explanation, :_score, :_version, :highlight, :sort)
-        end
-      end
-    end
-
-    def remove(*args)
-      if args.size > 1
-        type, document = args
-        type           = Utils.escape(type)
-        id             = get_id_from_document(document) || document
-      else
-        document = args.pop
-        type     = get_type_from_document(document)
-        id       = get_id_from_document(document) || document
-      end
-      raise ArgumentError, "Please pass a document ID" unless id
-
-      url    = "#{self.url}/#{type}/#{id}"
-      result = Configuration.client.delete url
-      MultiJson.decode(result.body) if result.success?
-
-    ensure
-      curl = %Q|curl -X DELETE "#{url}"|
-      logged(id, curl)
-    end
-
-    def retrieve(type, id)
-      raise ArgumentError, "Please pass a document ID" unless id
-
-      type      = Utils.escape(type)
-      url       = "#{self.url}/#{type}/#{id}"
-      @response = Configuration.client.get url
-
-      h = MultiJson.decode(@response.body)
-      if Configuration.wrapper == Hash then h
-      else
-        return nil if h['exists'] == false
-        document = h['_source'] || h['fields'] || {}
-        document.update('id' => h['_id'], '_type' => h['_type'], '_index' => h['_index'], '_version' => h['_version'])
-        Configuration.wrapper.new(document)
-      end
-
-    ensure
-      curl = %Q|curl -X GET "#{url}"|
-      logged(id, curl)
-    end
-
-    def refresh
-      @response = Configuration.client.post "#{url}/_refresh", ''
-
-    ensure
-      curl = %Q|curl -X POST "#{url}/_refresh"|
-      logged('_refresh', curl)
-    end
-
-    def open(options={})
-      # TODO: Remove the duplication in the execute > rescue > ensure chain
-      @response = Configuration.client.post "#{url}/_open", MultiJson.encode(options)
-      MultiJson.decode(@response.body)['ok']
-
-    ensure
-      curl = %Q|curl -X POST "#{url}/_open"|
-      logged('_open', curl)
-    end
-
-    def close(options={})
-      @response = Configuration.client.post "#{url}/_close", MultiJson.encode(options)
-      MultiJson.decode(@response.body)['ok']
-
-    ensure
-      curl = %Q|curl -X POST "#{url}/_close"|
-      logged('_close', curl)
-    end
-
-    def analyze(text, options={})
-      options = {:pretty => true}.update(options)
-      params  = options.to_param
-      @response = Configuration.client.get "#{url}/_analyze?#{params}", text
-      @response.success? ? MultiJson.decode(@response.body) : false
-
-    ensure
-      curl = %Q|curl -X GET "#{url}/_analyze?#{params}" -d '#{text}'|
-      logged('_analyze', curl)
-    end
-
-    def register_percolator_query(name, options={}, &block)
-      options[:query] = Search::Query.new(&block).to_hash if block_given?
-
-      @response = Configuration.client.put "#{Configuration.url}/_percolator/#{@name}/#{name}", MultiJson.encode(options)
-      MultiJson.decode(@response.body)['ok']
-
-    ensure
-      curl = %Q|curl -X PUT "#{Configuration.url}/_percolator/#{@name}/?pretty=1" -d '#{MultiJson.encode(options)}'|
-      logged('_percolator', curl)
-    end
-
-    def unregister_percolator_query(name)
-      @response = Configuration.client.delete "#{Configuration.url}/_percolator/#{@name}/#{name}"
-      MultiJson.decode(@response.body)['ok']
-
-    ensure
-      curl = %Q|curl -X DELETE "#{Configuration.url}/_percolator/#{@name}"|
-      logged('_percolator', curl)
-    end
-
-    def percolate(*args, &block)
-      document = args.shift
-      type     = get_type_from_document(document)
-
-      document = MultiJson.decode convert_document_to_json(document)
-
-      query = Search::Query.new(&block).to_hash if block_given?
-
-      payload = { :doc => document }
-      payload.update( :query => query ) if query
-
-      @response = Configuration.client.get "#{url}/#{type}/_percolate", MultiJson.encode(payload)
-      MultiJson.decode(@response.body)['matches']
-
-    ensure
-      curl = %Q|curl -X GET "#{url}/#{type}/_percolate?pretty=1" -d '#{payload.to_json}'|
-      logged('_percolate', curl)
+      curl = %Q|curl -X PUT #{url}/_refresh|
+      logged('REFRESH', curl)
     end
 
     def logged(endpoint='/', curl='')
@@ -285,50 +127,6 @@ module Tire
         end
 
         Configuration.logger.log_response code, nil, body
-      end
-    end
-
-    def get_type_from_document(document, options={})
-      options = {:escape => true}.merge(options)
-
-      old_verbose, $VERBOSE = $VERBOSE, nil # Silence Object#type deprecation warnings
-      type = case
-      when document.respond_to?(:document_type)
-        document.document_type
-      when document.is_a?(Hash)
-        document[:_type] || document['_type'] || document[:type] || document['type']
-      when document.respond_to?(:_type)
-        document._type
-      when document.respond_to?(:type) && document.type != document.class
-        document.type
-      end
-      $VERBOSE = old_verbose
-
-      type ||= 'document'
-      options[:escape] ? Utils.escape(type) : type
-    end
-
-    def get_id_from_document(document)
-      old_verbose, $VERBOSE = $VERBOSE, nil # Silence Object#id deprecation warnings
-      id = case
-      when document.is_a?(Hash)
-        document[:_id] || document['_id'] || document[:id] || document['id']
-      when document.respond_to?(:id) && document.id != document.object_id
-        document.id
-      end
-      $VERBOSE = old_verbose
-      id
-    end
-
-    def convert_document_to_json(document)
-      document = case
-      when document.is_a?(String)
-        Tire.warn "Passing the document as JSON string in Index#store has been deprecated, " +
-          "please pass an object which responds to `to_indexed_json` or a plain Hash."
-        document
-      when document.respond_to?(:to_indexed_json) then document.to_indexed_json
-      else raise ArgumentError, "Please pass a JSON string or object with a 'to_indexed_json' method," +
-          "'#{document.class}' given."
       end
     end
 
